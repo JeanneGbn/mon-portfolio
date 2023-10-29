80036b6fab5c42d1a8b3658bc59079fa642146a4
JeanneGbn <j.goubin@gmail.com>
2023-10-29 10:31:31 +0100

    deploy

--- src/Components/footer.js
+++ src/Components/footer.js
@@ -7,9 +7,7 @@ function Footer() {
             <div className='footer_left'>
               Mail: j.goubin@gmail.com
             </div>
-            <div className='footer_right'>
-            https://fr.linkedin.com/in/jeanne-goubin-a23a88250
-            </div>
+
         </footer>
     )
 }
